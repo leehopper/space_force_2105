3faca7cd7d1bd15670193add07f1ac4bfcda9c7b
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 09:15:26 -0600

    Build test for add specialties

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -15,4 +15,11 @@ describe Person do
     expect(@kathy.experience).to eq(10)
     expect(@kathy.specialties).to eq([])
   end
+
+  it 'adds specialties' do
+    @kathy.add_specialty(:astrophysics)
+    @kathy.add_specialty(:quantum_mechanics)
+
+    expect(@kathy.specialties).to eq([:astrophysics, :quantum_mechanics])
+  end
 end

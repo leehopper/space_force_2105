bba4347d9bf037eac3102bf234e09ae771a68ad0
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 09:20:21 -0600

    Build test for requirements and add requirements

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -13,5 +13,13 @@ describe Spacecraft do
   it 'attributes' do
     expect(@daedalus.name).to eq("Daedalus")
     expect(@daedalus.fuel).to eq(400)
+    expect(@daedalus.requirements).to eq([])
+  end
+
+  it 'adds requirements' do
+    daedalus.add_requirement({astrophysics: 6})
+    daedalus.add_requirement({quantum_mechanics: 3})
+
+    expect(@daedalus.requirements).to eq([{astrophysics: 6}, {quantum_mechanics: 3}])
   end
 end

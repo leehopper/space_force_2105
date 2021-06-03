f2d99fd7ae27d3e76d44ecf6fcfa85c530fbad96
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 09:22:16 -0600

    Build add requirement method and requirements instance variable

--- lib/spacecraft.rb
+++ lib/spacecraft.rb
@@ -1,8 +1,13 @@
 class Spacecraft
-  attr_reader :name, :fuel
+  attr_reader :name, :fuel, :requirements
 
   def initialize(ship)
     @name = ship[:name]
     @fuel = ship[:fuel]
+    @requirements = []
+  end
+
+  def add_requirement(requirement)
+    @requirements << requirement
   end
 end

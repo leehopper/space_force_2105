51b6ee00b4518315ddf87ba872e6f7af36150c6d
Lee Hopper <hopper.lee.j@gmail.com>
2021-06-03 09:08:05 -0600

    Build test for attributes

--- spec/spacecraft_spec.rb
+++ spec/spacecraft_spec.rb
@@ -3,10 +3,15 @@ require './lib/spacecraft'
 
 describe Spacecraft do
   before (:each) do
-    @aedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
+    @daedalus = Spacecraft.new({name: 'Daedalus', fuel: 400})
   end
 
   it 'exists' do
-    expect(@aedalus).to be_a(Spacecraft)
+    expect(@daedalus).to be_a(Spacecraft)
+  end
+
+  it 'attributes' do
+    expect(@daedalus.name).to eq("Daedalus")
+    expect(@daedalus.fuel).to eq(400)
   end
 end
